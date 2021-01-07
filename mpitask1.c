64d3c002528e93ae83fb58ca9cb9bc3295f86ffa
xhx0095 <71507775+xhx0095@users.noreply.github.com>
2021-01-08 03:25:48 +0800

    Update mpitask1.c

--- mpitask1.c
+++ mpitask1.c
@@ -2,52 +2,49 @@
 #include "mpi.h"
 #include<malloc.h>
 main(int argc, char** argv) {
-	int my_rank; 
-	int p; 
-	int source; 
-	int dest; 
-	int tag = 50; 
-	char message[100]; 
-	
+	int my_rank;
+	int p;
+	int source;
+	int dest;
+	int tag = 50;
+	char message[1000];
+	int count;
 	int data1;
-	int data2;
-	MPI_Status status; 
+	
+	MPI_Status status;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 	if (my_rank != 0) {
-		sprintf(message, "hellow from process %d!",
+		sprintf(message,"is %d!",
 			my_rank);
-		data1 = my_rank + 2;
-		data2 = my_rank + 3;
-		dest = 0;
+		data1 = my_rank;
 		
-		MPI_Send(message, strlen(message) + 1, MPI_CHAR, dest,
-			tag, MPI_COMM_WORLD);
+		dest = 0;
+
+		
 		MPI_Send(&data1, 2, MPI_INT, dest, tag, MPI_COMM_WORLD);
-		MPI_Send(&data2, 2, MPI_INT, dest, tag, MPI_COMM_WORLD);
+		
 
 	}
-	
-	
+
+
 
 	if (my_rank == 0) {
-		int(*a)[2] = (int(*)[2])malloc(sizeof(int) * 4 * 2);
+		int(*a)[2] = (int(*)[2])malloc(sizeof(int) * 6 * 2);
+		
+		    
 		for (source = 1; source < p; source++) {
+			MPI_Probe(MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
+			MPI_Get_count(&status, MPI_INT, &count);
 			
-				MPI_Recv(message, 100, MPI_CHAR, source, tag,
-					MPI_COMM_WORLD, &status);
-				MPI_Recv(&data1, 2, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
-				MPI_Recv(&data2, 2, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
-				a[source][0] = data1;
-				a[source][1] = data2;
-			printf("%s\n", message);
-			printf("%d,%d\n",a[source][0], a[source][1]);
-		}
+			MPI_Recv(&data1, 2, MPI_INT, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
+			
+			a[source][0] = data1;
+			
+			
+			printf(" help from %d\n", a[source][0]);
+			
 
-		
-		
-		
-	}
-	MPI_Finalize();
-}
+
+		}
