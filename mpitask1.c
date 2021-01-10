d82b7c13cffde4a5fabc024bea7cb72486979fd6
xhx0095 <71507775+xhx0095@users.noreply.github.com>
2021-01-10 16:10:30 +0800

    Update mpitask1.c

--- mpitask1.c
+++ mpitask1.c
@@ -10,41 +10,26 @@ main(int argc, char** argv) {
 	char message[1000];
 	int count;
 	int data1;
-	
+
 	MPI_Status status;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 	if (my_rank != 0) {
-		sprintf(message,"is %d!",
-			my_rank);
 		data1 = my_rank;
-		
 		dest = 0;
-
-		
 		MPI_Send(&data1, 2, MPI_INT, dest, tag, MPI_COMM_WORLD);
-		
-
 	}
-
-
-
 	if (my_rank == 0) {
-		int(*a)[2] = (int(*)[2])malloc(sizeof(int) * 6 * 2);
-		
-		    
 		for (source = 1; source < p; source++) {
 			MPI_Probe(MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
 			MPI_Get_count(&status, MPI_INT, &count);
-			
+			printf(" count is %d\n", count);
+			int(*a)[2] = (int(*)[2])malloc(sizeof(int) * count * 2);
 			MPI_Recv(&data1, 2, MPI_INT, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
-			
 			a[source][0] = data1;
-			
-			
 			printf(" help from %d\n", a[source][0]);
-			
-
-
 		}
+	}
+	MPI_Finalize();
+}
